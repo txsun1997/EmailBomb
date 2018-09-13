8ea5885d7cceb1c091b920172be2d3a57e5e95b4
Tianxiang Sun <txsun1997@gmail.com>
2018-09-13 18:01:37 +0800

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,15 +1,8 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Sep  7 18:42:23 2018
-
-@author: lenovo
-"""
 
 import tkinter as tk
-import sendInfo
-
-def attach():
-    pass
+import sendInfo, config
+import tkinter.filedialog
 
 def send():
     message = contentText.get('1.0',tk.END)
@@ -17,14 +10,22 @@ def send():
     toAddr = toAddrEntry.get()
     num = int(numEntry.get())
     fromAddr = fromAddrEntry.get()
-    sendInfo.sendLoop(title,message,fromAddr,toAddr,num)
+    filePath = filePathEntry.get()
+    sendInfo.sendLoop(title,message,fromAddr,toAddr,num,filePath)
+    state.set('已成功发送' + str(config.succNum) + '封邮件')
 
-def exitP():
-    pass
+def login():
+    server = sendInfo.connect()
+    state.set(config.globalStr)
 
+def attach():
+    filename = tkinter.filedialog.askopenfilename()
+    if filename != '':
+        filePath.set(filename);
+	
 window = tk.Tk()
 window.title("基于SMTP协议的邮件伪造和邮件炸弹")
-window.geometry('450x300')
+window.geometry('450x320')
         
 tk.Label(window, text='   发件箱:').place(x=20, y=20)
 tk.Label(window, text='   收件箱:').place(x=20, y=45)
@@ -32,7 +33,13 @@ tk.Label(window, text='邮件标题:').place(x=20, y=70)
 tk.Label(window, text='邮件内容:').place(x=20, y=95)
 tk.Label(window, text='发送数量:').place(x=20, y=185)
 tk.Label(window, text='添加附件').place(x=20, y=210)
-        
+tk.Label(window, text='当前状态:').place(x=20, y=240)
+
+state = tk.StringVar()
+state.set(config.globalStr)
+stateLabel = tk.Label(window, textvariable=state, fg='red')
+stateLabel.place(x=80, y=240)
+
 fromAddr = tk.StringVar()
 fromAddr.set(" [请输入发件人地址，如xxxxx@example.com]")
 fromAddrEntry = tk.Entry(window, textvariable = fromAddr, width = 40)
@@ -52,30 +59,25 @@ contentText = tk.Text(window, width = 40, height = 6)
 contentText.place(x=80, y=95)
 contentText.insert(tk.END, " 邮件内容...")
 
+filePath = tk.StringVar()
+filePathEntry = tk.Entry(window, textvariable = filePath, width = 35)
+filePathEntry.place(x=80, y=210)
+     
 attachBtn =  tk.Button(window, text='浏览', command=attach)
-attachBtn.place(x=80, y=207)        
+attachBtn.place(x=330, y=207)        
         
 sendNum = tk.StringVar()
 numEntry = tk.Entry(window, textvariable = sendNum, width = 20)
 numEntry.place(x=80, y=185)
         
+sendBtn = tk.Button(window, text='  登   录  ', command=login)
+sendBtn.place(x=100, y=280)
+
 sendBtn = tk.Button(window, text='发送邮件', command=send)
-sendBtn.place(x=150, y=250)
+sendBtn.place(x=176, y=280)
         
-exitBtn = tk.Button(window, text='退出程序', command=exitP)
-exitBtn.place(x=250, y=250)
-        
-curWidth = window.winfo_width()  # get current width
-curHeight = window.winfo_height()  # get current height
-scnWidth, scnHeight = window.maxsize()  # get screen width and height
-tmpcnf = '+%d+%d' % ((scnWidth - curWidth) / 2, (scnHeight - curHeight) / 2)
-window.geometry(tmpcnf)
+exitBtn = tk.Button(window, text='退出程序', command=window.destroy)
+exitBtn.place(x=250, y=280)
+
 window.mainloop()
-
-
-
-
-
-
-
     
